e2dee8192cd2c91f90a8fbb336d773d8632b426b
vacit <wajdabdulrazak@gmail.com>
2020-01-12 17:49:19 +0300

    Updates

---
+++ precache-manifest.2d171b35ec3e83e0147e911ea07a9e97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe03598c9f27799e5981a29d4115108",
+    "url": "/heads-or-tails.git/index.html"
+  },
+  {
+    "revision": "ef742eab5f0b86f4699c",
+    "url": "/heads-or-tails.git/static/css/main.6f842b94.chunk.css"
+  },
+  {
+    "revision": "a52fa2e3d750a5559fc2",
+    "url": "/heads-or-tails.git/static/js/2.c202c282.chunk.js"
+  },
+  {
+    "revision": "ef742eab5f0b86f4699c",
+    "url": "/heads-or-tails.git/static/js/main.473e52c8.chunk.js"
+  },
+  {
+    "revision": "9100d09b73e330fe3cf0",
+    "url": "/heads-or-tails.git/static/js/runtime~main.961983df.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/heads-or-tails.git/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/heads-or-tails.git/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
