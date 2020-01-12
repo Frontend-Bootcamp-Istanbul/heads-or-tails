1f7423dbed9f25f7737c8f78269b34c0a67395e2
vacit <wajdabdulrazak@gmail.com>
2020-01-12 16:27:14 +0300

    build folder added

---
+++ docs/precache-manifest.7edd8e659a53e928623921c3fb5d1fc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d08c7e70ce27168ea1d04190dc907a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96941706832af85bf4f4",
+    "url": "/static/css/main.6f842b94.chunk.css"
+  },
+  {
+    "revision": "a52fa2e3d750a5559fc2",
+    "url": "/static/js/2.c202c282.chunk.js"
+  },
+  {
+    "revision": "96941706832af85bf4f4",
+    "url": "/static/js/main.42f0a905.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
